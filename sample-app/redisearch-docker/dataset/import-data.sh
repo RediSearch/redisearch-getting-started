5b79cc0f05a9fd3514920b23541e9cf621e59b75
Jason Haugland <jasonhaugland@gmail.com>
2022-06-14 09:37:20 -0500

    run batch load using separate docker container

--- sample-app/redisearch-docker/dataset/import-data.sh
+++ sample-app/redisearch-docker/dataset/import-data.sh
@@ -3,5 +3,4 @@ redis-cli -h redisearch -p 6379 < /dataset/import_actors.redis
 redis-cli -h redisearch -p 6379 < /dataset/import_movies.redis
 redis-cli -h redisearch -p 6379 < /dataset/import_users.redis
 redis-cli -h redisearch -p 6379 < /dataset/import_create_index.redis
-redis-cli shutdown
 exit
