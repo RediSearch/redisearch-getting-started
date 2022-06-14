1782be3cb5076147ad77883176913fabac2a57b8
Jason Haugland <jasonhaugland@gmail.com>
2022-06-14 09:30:18 -0500

    run batch load using separate docker container

--- sample-app/redisearch-docker/dataset/import-data.sh
+++ sample-app/redisearch-docker/dataset/import-data.sh
@@ -1,17 +1,7 @@
-
-
-redis-server --loadmodule /usr/lib/redis/modules/redisearch.so --daemonize yes && sleep 2
-
-redis-cli -p 6379 < /data/import_actors.redis
-
-redis-cli -p 6379 < /data/import_movies.redis
-
-redis-cli -p 6379 < /data/import_users.redis
-
-redis-cli -p 6379 < /data/import_create_index.redis
-
-redis-cli save 
-
-redis-cli shutdown 
-
-redis-server --loadmodule /usr/lib/redis/modules/redisearch.so 
+sleep 15
+redis-cli -h redisearch -p 6379 < /dataset/import_actors.redis
+redis-cli -h redisearch -p 6379 < /dataset/import_movies.redis
+redis-cli -h redisearch -p 6379 < /dataset/import_users.redis
+redis-cli -h redisearch -p 6379 < /dataset/import_create_index.redis
+redis-cli shutdown
+exit
